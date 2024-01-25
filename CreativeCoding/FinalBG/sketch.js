2110360db3ca77f8bbd29c37149c8c58fbfcdd97
Rglanville32006 <145369232+Rglanville32006@users.noreply.github.com>
2024-01-25 10:40:25 -0500

    update

--- CreativeCoding/FinalBG/sketch.js
+++ CreativeCoding/FinalBG/sketch.js
@@ -1,30 +1,35 @@
-function setup() {
-  createCanvas(400, 400);
-}
 
-function draw() {
-  background(220);
-}
 
 let gif; 
 
 function preload() {
-createCanvas(windowWidth, windowHeight);
+gif = loadImage ('assets/GIFtransparent.gif');
 }
-function () {
+
+function setup () {
+
+createCanvas(windowWidth, windowHeight); 
+
+
+}
+
+function draw() { 
+
 
 background ('red');
+
 var num = 50;
 varsideLen = windowWidth/num;
 
 for (var y = 0; y < windowHeight; y = y + sideLen) {
 
+
 for  (var x = 0; x < windowHeight; x = x + sideLen) {
 image (gif, x, y, sideLen, sideLen);
+}
+}
+}
 
-}
-}
-}
 function windowResized(){
 resizeCanvas (windowWidth, windowHeight); 
 }
